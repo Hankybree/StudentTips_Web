7883c83989bc11e4c72ef4e58f0d80a8842d70d4
Henrik Frank <frank.havsvinge@gmail.com>
2020-05-20 15:14:50 +0200

    Set up VueRouter and Vuex in project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
+
+const router = new VueRouter({
+  routes: {
+    // VueRouter routes goes here
+  }
+})
+
+const store = new Vuex({
+  state: {
+    // Vuex variables goes here
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
