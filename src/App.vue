77c051a7a46730a4864f839a2626814419d55ee8
Marcus Nilsson <marcusnilsson93@yahoo.com>
2020-05-22 17:02:16 +0200

    wirefraneWebb

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header id="head">
+      <p>Jag testar nu fonten</p>
+      <!--Header content-->
+      <!--maybe some sort of navigation menu-->
+    </header>
+    <!--a logo that we have created?-->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="'Welcome to'" />
+    <footer id="footer">
+      <!--footer content copywrite text?-->
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
+#head {
+  height: 2em;
+  background-color: #bd5000;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Raleway;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+#footer {
+  position: absolute;
+  margin: 0.25em;
+  bottom: 0px;
+  height: 25px;
+  width: 100%;
+  background-color: #ffb042;
+}
+</style> 
