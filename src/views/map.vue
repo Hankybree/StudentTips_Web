fca95424004d9b0331d18033bdab22257c8e4251
Marcus Nilsson <marcusnilsson93@yahoo.com>
2020-05-22 17:01:11 +0200

    wireframe

---
+++ src/views/map.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="map">
+    <!--Här skall kartan/kartviwn inplementeras, man skulle ochskå kunna ha lite eventulla knappar för olika inställningar,
+    dock är det nog en sak som kommer med mapbox om jag inte minns fel-->
+  </div>
+</template>
+
+<style scoped>
+/*Style mapview here*/
+</style>
