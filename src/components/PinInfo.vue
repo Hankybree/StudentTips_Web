366e02a0159a974a7dffba67463e4fad2cc75103
jakob salomonsson <jakob@salomonssons.se>
2020-06-04 11:32:22 +0200

    fixat bild cropping hyfsat

--- src/components/PinInfo.vue
+++ src/components/PinInfo.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="PinInfo"></div>
+  <div class="PinInfo">
+    <div id="img-container">
+      <img src="../assets/kockjakob.jpg" alt="hej" />
+    </div>Add image
+    <input id="pin-image" type="file" name="pinImage" />
+
+    <ul>
+      <li>
+        <input placeholder="Användarnamn" type="text" name="userName" id="user-name" />
+      </li>
+      <li>
+        <input placeholder="Användarnamn" type="text" name="userName" id="user-name" />
+      </li>
+      <li>
+        <input placeholder="Användarnamn" type="text" name="userName" id="user-name" />
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+//import { computed } from "../scripts/computed.js";
+
 export default {
   name: "PinInfo"
 };
@@ -10,9 +29,25 @@ export default {
 .PinInfo {
   display: flex;
   flex-direction: column;
-  border: 2px dotted tomato;
+  align-content: space-between;
+  flex-wrap: flex;
   background-color: teal;
-  width: 25vw;
-  height: 25vh;
+  width: 15vw;
+  height: 75vh;
+  border-radius: 8px;
+}
+
+.PinInfo > div {
+  border-radius: 8px;
+  background-color: yellow;
+  height: 130px;
+  width: 216px;
+}
+.PinInfo > div > img {
+  object-fit: cover;
+  object-position: -50% 0;
+
+  width: 100%;
+  height: 100%;
 }
 </style>
